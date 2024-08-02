ebd88e767c4afa2efa821d3fa141a556055743b6
tyuukau <bachocnhi2002@gmail.com>
2024-08-02 10:28:20 +0700

    [code] ~ run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -25,7 +25,7 @@ done
 
 # Run the Python app
 if [[ -n $NGROK ]]; then
-    python -m rag_chatbot --host localhost & ngrok http 7860
+    python -m rag_legal_chatbot --host localhost & ngrok http 7860
 else
-    python -m rag_chatbot --host localhost
+    python -m rag_legal_chatbot --host localhost
 fi
